4ddc7fd877cae17dd7ddace19a835fd0403fc1f0
Malti Pathak <164329815+Maltipat@users.noreply.github.com>
2025-08-23 00:17:44 +0530

    Create secondmethode.java

---
+++ secondmethode.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^nums[i];
+        }
+        
+        return xor;
+    }
+}
